a952ce8f482fd1e0598eab75ed4893048b167a7b
Will Papp <wpapp90@gmail.com>
2021-04-28 17:58:09 -0500

    fixed seeding issue

--- models/Workout.js
+++ models/Workout.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-   workouts: [
+    exercises: [
        {
            type: {
                type: String,
